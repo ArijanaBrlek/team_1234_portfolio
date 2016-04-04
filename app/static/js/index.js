075bfb5c7798f2dd07ade5805ba1857143b1fdeb
Arijana Brlek <arijanabrlek@gmail.com>
2016-04-04 17:25:34 +0200

    Add colors to home page

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,3 +1,14 @@
+$(document).ready(function(){
+
+    createGrid(125);
+    loadTemplates();
+});
+
+$(window).resize(function() {
+    createGrid(125);
+});
+
+
 function createGrid(size) {
     var ratioW = Math.floor($(window).width()/size),
         ratioH = Math.floor($(window).height()/size);
@@ -11,18 +22,43 @@ function createGrid(size) {
 
     var leftoverSize = ratioW * size % (size -1);
 
-    for (var i = 0; i < ratioH + 1; i++) {
-        for(var p = 0; p < ratioW + 1; p++){
+    var people = [
+    	{id: 1, x: 2, y: 5},
+    	{id: 2, x: 2, y: 6},
+    	{id: 3, x: 2, y: 7},
+    	{id: 4, x: 2, y: 8},
+    ];
+
+    var cellsInRow = ratioH + 1;
+    var cellsInColumn = ratioW + 1;
+
+    // @TODO: make sure generated positions are unique
+    for(var i = 0; i < people.length; ++i) {
+    	people[i].x = randomIntFromInterval(1, cellsInRow - 1);
+    	people[i].y = randomIntFromInterval(1, cellsInColumn - 1);
+    }
+
+    for (var i = 0; i < cellsInRow; i++) {
+        for(var p = 0; p < cellsInColumn; p++){
             $('<div />', {
                 width: size - 1, 
                 height: size - 1
-            }).addClass('cell').appendTo(parent);
+            }).addClass('cell')
+			.attr("data-x", i)
+			.attr("data-y", p)
+            .appendTo(parent);
         }
     }
+
+    for (var i = people.length - 1; i >= 0; i--) {
+    	var member = people[i];
+    	$('.cell[data-x="' + member.x + '"][data-y="' + member.y + '"]')
+    	.addClass('member_cell_' + member.id);
+    };
 }
 
-createGrid(125);
 
-$(window).resize(function() {
-	createGrid(125);
-});
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min)+min);
+}
